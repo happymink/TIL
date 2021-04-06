45501b526eb5010c751b180ed4fb6222d25f965e
xxunghee <xxunghee@gmail.com>
2021-04-06 13:29:38 +0900

    [UPDATE] static method's variable

--- Java/keyword_static/src/com/company/Test.java
+++ Java/keyword_static/src/com/company/Test.java
@@ -1,12 +1,17 @@
 package com.company;
 
 public class Test implements Contract{
-    public static void staticTest() {
+    static int sa;
+    int a;
 
+    public static void staticTest() {
+        sa = 1; // OK
+        a = 1;  // ERROR : static method 생성 시 a가 할당되지 않았을 수 있음 => static method는 non-static variable 접근 불가
     }
 
     public void nonStaticTest() {
-
+        sa = 2; // OK
+        a = 2;  // OK
     }
 
     @Override
