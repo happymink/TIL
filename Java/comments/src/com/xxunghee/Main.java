6f6867f4ba05ee2ac07649b769c7cee72f95c4f8
xxunghee <xxunghee@gmail.com>
2021-04-07 14:33:09 +0900

    [UPDATE] sample code

--- Java/comments/src/com/xxunghee/Main.java
+++ Java/comments/src/com/xxunghee/Main.java
@@ -1,8 +1,14 @@
 package com.xxunghee;
 
 public class Main {
+    public static int add(int a, int b) {
+        return a+b;
+    }
 
     public static void main(String[] args) {
+        int a = 97;
+        int b = 98;
 
+        System.out.println("a + b = " + add(a, b));
     }
 }
