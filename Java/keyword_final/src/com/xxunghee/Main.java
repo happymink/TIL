cac789cd2c681e4f9a4da4f3b3e94a2ce9a65f9d
xxunghee <xxunghee@gmail.com>
2021-04-07 09:55:09 +0900

    [UPDATE] final variable

--- Java/keyword_final/src/com/xxunghee/Main.java
+++ Java/keyword_final/src/com/xxunghee/Main.java
@@ -1,8 +1,20 @@
 package com.xxunghee;
 
 public class Main {
-
     public static void main(String[] args) {
+        final String s1 = "hello";
+        String s2 = "hello";
 
+        s1 = "Jello";   // ERROR : final 키워드가 붙은 변수는 값의 변경 불가능
+        s2 = "Yellow";  // OK
+
+        final int fa;
+        int a;
+
+        fa = 0; // 값 할당
+        a = 0;  // 초기값 할당
+
+        fa = 3; // ERROR : final 키워드가 붙은 변수는 값의 변경 불가능
+        a = 3;  // OK
     }
 }
