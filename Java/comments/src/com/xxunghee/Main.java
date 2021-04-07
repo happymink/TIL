9850dfff595519115a7e680685ddf30a5bf52758
xxunghee <xxunghee@gmail.com>
2021-04-07 14:34:31 +0900

    [UPDATE] block comment

--- Java/comments/src/com/xxunghee/Main.java
+++ Java/comments/src/com/xxunghee/Main.java
@@ -9,6 +9,7 @@ public class Main {
         int a = 97; // 'a'의 ASCII 코드 값
         int b = 98; // 'b'의 ASCII 코드 값
 
+        /* a +b 값 출력하기 */
         System.out.println("a + b = " + add(a, b));
     }
 }
