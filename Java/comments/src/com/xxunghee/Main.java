81141675a4e9fbb630f2700b482a72940aa8b513
xxunghee <xxunghee@gmail.com>
2021-04-07 14:33:41 +0900

    [UPDATE] line comment

--- Java/comments/src/com/xxunghee/Main.java
+++ Java/comments/src/com/xxunghee/Main.java
@@ -6,8 +6,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int a = 97;
-        int b = 98;
+        int a = 97; // 'a'의 ASCII 코드 값
+        int b = 98; // 'b'의 ASCII 코드 값
 
         System.out.println("a + b = " + add(a, b));
     }
