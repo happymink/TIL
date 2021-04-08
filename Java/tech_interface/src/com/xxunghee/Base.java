5174f095a8afddda5d40c74eff7c6aa70323e9ce
xxunghee <xxunghee@gmail.com>
2021-04-08 13:08:24 +0900

    [UPDATE] interface inheritance

---
+++ Java/tech_interface/src/com/xxunghee/Base.java
@@ -0,0 +1,8 @@
+package com.xxunghee;
+
+public class Base implements BaseInterface{
+    @Override
+    public void baseFunc() {
+        System.out.println("Call Base class's baseFunc()");
+    }
+}
--- Java/tech_interface/src/com/xxunghee/Inner.java
+++ Java/tech_interface/src/com/xxunghee/Inner.java
@@ -1,13 +1,18 @@
 package com.xxunghee;
 
-public class Inner implements OuterInterface.InnerInterface{
+public class Inner extends Base implements OuterInterface.InnerInterface{
     @Override
     public void inner() {
         System.out.println("call " + OuterInterface.InnerInterface.loc);
     }
 
+    /**
+     * 부모 클래스에서 구현이 되어 있으므로 {@link Base#baseFunc()} 오버라이딩하지 않아도 됨
+     * 이 코드처럼 구현 내용이 다른 경우 오버라이딩
+     */
     @Override
     public void baseFunc() {
         // 상속받은 인터페이스의 메소드도 오버라이딩 해야함
+        System.out.println("Call Inner class's baseFunc()");
     }
 }
