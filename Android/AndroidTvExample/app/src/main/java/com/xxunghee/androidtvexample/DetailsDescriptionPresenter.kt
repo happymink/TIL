341b790d89de20b1a5b945f3d6fd9d0a3a3d4f0e
xxunghee <xxunghee@gmail.com>
2021-08-13 16:10:31 +0900

    Initial commit

---
+++ Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/DetailsDescriptionPresenter.kt
@@ -0,0 +1,14 @@
+package com.xxunghee.androidtvexample
+
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+
+    override fun onBindDescription(viewHolder: ViewHolder, item: Any) {
+        val movie = item as Movie
+
+        viewHolder.title.text = movie.title
+        viewHolder.subtitle.text = movie.studio
+        viewHolder.body.text = movie.description
+    }
+}
