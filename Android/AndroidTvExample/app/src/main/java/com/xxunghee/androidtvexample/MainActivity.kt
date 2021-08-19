74e8f3fe0392e44d1b67fc011aa003d1b11d97fe
xxunghee <xxunghee@gmail.com>
2021-08-19 13:29:35 +0900

    [ADD] Show keyCode in toast message

--- Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/MainActivity.kt
+++ Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.xxunghee.androidtvexample
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 
 /**
  * Loads [MainFragment].
  */
 class MainActivity : FragmentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +17,11 @@ class MainActivity : FragmentActivity() {
                 .replace(R.id.main_browse_fragment, MainFragment())
                 .commitNow()
         }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Toast.makeText(this, "$keyCode", Toast.LENGTH_SHORT).show()
+
+        return super.onKeyDown(keyCode, event)
     }
 }
