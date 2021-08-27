959abcf6155c091a538fe93a18753f03785fb38f
xxunghee <xxunghee@gmail.com>
2021-08-27 14:06:31 +0900

    [MODIFY] Change video by up/down button

--- Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/view/activity/PlaybackActivity.kt
+++ Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/view/activity/PlaybackActivity.kt
@@ -3,11 +3,16 @@ package com.xxunghee.androidtvexample.view.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.KeyEvent
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.xxunghee.androidtvexample.view.fragment.Channel1Fragment
+import com.xxunghee.androidtvexample.view.fragment.Channel2Fragment
 import com.xxunghee.androidtvexample.view.fragment.PlaybackVideoFragment
 
 /** Loads [PlaybackVideoFragment]. */
 class PlaybackActivity : FragmentActivity() {
+    private val channels = listOf<Fragment>(PlaybackVideoFragment(), Channel1Fragment(), Channel2Fragment())
+    private var channel = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +26,22 @@ class PlaybackActivity : FragmentActivity() {
     /**
      * DPAD 키 이벤트 설정
      *
-     * up(^), down(v), center(ㅇ) 키 이벤트 비활성화
+     * up(^), down(v): 채널 변경
      */
     @SuppressLint("RestrictedApi")
     override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
-        return when (event?.keyCode) {
-            KeyEvent.KEYCODE_DPAD_UP,
-            KeyEvent.KEYCODE_DPAD_DOWN,
-            KeyEvent.KEYCODE_DPAD_CENTER -> true
-            else -> super.dispatchKeyEvent(event)
+        return if (event?.keyCode == KeyEvent.KEYCODE_DPAD_UP && event.action == KeyEvent.ACTION_DOWN) {
+            if (++channel > channels.size) channel -= channels.size
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, channels[channel]).commit()
+            true
+        } else if (event?.keyCode == KeyEvent.KEYCODE_DPAD_DOWN && event.action == KeyEvent.ACTION_DOWN) {
+            if (--channel < 0) channel += channels.size
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, channels[channel]).commit()
+            true
+        } else {
+            super.dispatchKeyEvent(event)
         }
     }
 }
--- Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/view/fragment/PlaybackVideoFragment.kt
+++ Android/AndroidTvExample/app/src/main/java/com/xxunghee/androidtvexample/view/fragment/PlaybackVideoFragment.kt
@@ -2,6 +2,7 @@ package com.xxunghee.androidtvexample.view.fragment
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.leanback.app.VideoSupportFragment
 import androidx.leanback.app.VideoSupportFragmentGlueHost
 import androidx.leanback.media.MediaPlayerAdapter
@@ -9,6 +10,7 @@ import androidx.leanback.media.PlaybackTransportControlGlue
 import androidx.leanback.widget.PlaybackControlsRow
 import com.xxunghee.androidtvexample.util.CustomGlue
 import com.xxunghee.androidtvexample.data.Movie
+import com.xxunghee.androidtvexample.data.MovieList
 import com.xxunghee.androidtvexample.view.activity.DetailsActivity
 
 /**
@@ -20,12 +22,14 @@ class PlaybackVideoFragment : VideoSupportFragment() {
 
     private lateinit var mTransportControlGlue: PlaybackTransportControlGlue<MediaPlayerAdapter>
 
-    private var title: String? = "시작 비디오"
-    private var description: String? = "시작 비디오 설명"
-    private var videoUrl: String? = "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4"
+    private var title: String? = MovieList.list[0].title
+    private var description: String? = MovieList.list[0].description
+    private var videoUrl: String? = MovieList.list[0].videoUrl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Toast.makeText(context, this.javaClass.simpleName, Toast.LENGTH_SHORT).show()
 
         (activity?.intent?.getSerializableExtra(DetailsActivity.MOVIE) as? Movie)?.let {
             title = it.title
